1feed7fa798e892248f7527e9a27eedea70afb11
tbz <2070957476>
2025-04-01 17:54:49 +0800

    init a rc-field-form

--- app/form/page.tsx
+++ app/form/page.tsx
@@ -1,63 +1,51 @@
-"use client";
+'use client';
 
-import React, {Component} from "react";
-// import {createForm} from "rc-form";
-import createForm from "../ui/myForm";
+import React from 'react';
+import Form, { Field } from 'rc-field-form';
+import Input from '../ui/input';
 
+const nameRules = { required: true, message: '请输入姓名！' };
+const nameValidator = {
+  validator: (_: any, value: string) => {
+    if (!/^[\u4e00-\u9fa5]{1,6}$/.test(value)) {
+      return Promise.reject('用户名只允许为六位以内中文');
+    }
+    return Promise.resolve();
+  },
+};
+const passwordRules = { required: true, message: '请输入密码！' };
 
-import Input from "../ui/input";
+const MyRCForm = () => {
+  const [form] = Form.useForm();
 
-const nameRules = {required: true, message: "请输入姓名！"};
-const nameValidator = { validator: (value: string) => {
-  if (!/^[\u4e00-\u9fa5]{1,6}$/.test(value)) {
-    return "用户名只允许为六位以内中文";
-  }
-}}
-const passwordRules = {required: true, message: "请输入密码！"};
+  React.useEffect(() => {
+    form.setFieldsValue({ username: 'default' });
+  }, [form]);
 
-class MyRCForm extends Component<any, any> {
-  constructor(props: any) {
-    super(props);
-    // this.state = {
-    //   username: "",
-    //   password: ""
-    // };
-  }
-
-  componentDidMount() {
-    this.props.form.setFieldsValue({username: "default"});
-  }
-
-  submit = () => {
-    const {validateFields} = this.props.form;
-    validateFields((err: any, val: any) => {
-      if (err) {
-        console.log("err", err); //sy-log
-      } else {
-        console.log("校验成功", val); //sy-log
-      }
-    });
+  const onFinish = (values: any) => {
+    console.log('校验成功', values);
   };
 
-  render() {
-    const {getFieldDecorator} = this.props.form;
-    return (
+  const onFinishFailed = ({ errorFields }: any) => {
+    console.log('err', errorFields);
+  };
+
+  return (
+    <Form form={form} onFinish={onFinish} onFinishFailed={onFinishFailed}>
       <div>
         <h3>MyRCFormPage</h3>
-        {getFieldDecorator("username", {rules: [nameRules, nameValidator]})(
+        <Field name="username" rules={[nameRules, nameValidator]}>
           <Input placeholder="Username" />
-        )}
-        {getFieldDecorator("password", {rules: [passwordRules]})(
+        </Field>
+
+        <Field name="password" rules={[passwordRules]}>
           <Input placeholder="Password" />
-        )}
+        </Field>
 
-        <button onClick={this.submit}>submit</button>
+        <button type="submit">submit</button>
       </div>
-    );
-  }
-}
+    </Form>
+  );
+};
 
-// HOC higher order component : 是个函数，但是接收组件作为参数，返回一个新的组件
-const MyRCFormPage = createForm()(MyRCForm);
-
-export default MyRCFormPage;
+export default MyRCForm;
